21a908ad3c2dad26834aa79a052977577a92df9c
Mahesh Jayaprakash <jmahesh2000@gamil.com>
2020-12-29 13:36:08 -0500

    updated java file

--- hello.java
+++ hello.java
@@ -1,7 +1,5 @@
-// Your First Program
-
-class HelloWorld {
+public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!"); 
+        System.out.println("Hello, this is Java");
     }
 }
